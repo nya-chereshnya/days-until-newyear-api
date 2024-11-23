321615796d00d39db4a28c38bfac88522f7753fc
Nya <nya@nyashka.com>
2024-11-23 19:18:46 +0400

    added middleware: User-Role:Admin detector

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
@@ -9,12 +10,21 @@ import (
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(MW())
 	r.GET("/", Handler)
 	return r
 }
 
+func MW() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if val := c.Request.Header.Get("User-Role"); val == "admin" {
+			log.Println("red button user detected")
+		}
+		c.Next()
+	}
+}
+
 func Handler(c *gin.Context) {
-	//	c.String(http.StatusOK, "test")
 	date := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
 	duration := time.Until(date)
 	c.String(http.StatusOK, "time until 01.01.2025: %d", int64(duration.Hours()/24))
